27cc6c98cb090f4021a30d5c5e3b37e79fb5ac31
algo7 <11154774+algo7@users.noreply.github.com>
2020-03-29 05:35:32 +0800

    adding text normalization func

--- config/textProcess.js
+++ config/textProcess.js
@@ -5,58 +5,53 @@
 const standardLex = require('apos-to-lex-form');
 const natural = require('natural');
 const stWord = require('stopword');
-const strSim=require('string-similarity');
+const strSim = require('string-similarity');
 
 const { WordTokenizer } = natural;
 const tokenizer = new WordTokenizer;
 
 //Load dictionary file
-const dict= require('../config/dict.js').data;
-// const ye=strSim.findBestMatch('love',dict)
-// const ye = strSim.findBestMatch('love',dict)
-// console.log(ye.bestMatch)
-
-// for (let index = 0; index < dict.length; index++) {
-//     const element = dict[index];
-//     console.log(element)
-// }
+const dict = require('../config/dict.js').data;
 
 //BestMatch
+const bestMatch = (array) => {
 
-const bestMatch=(array)=>{
-
-    let finalArray=[]
+    let finalArray = []
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
-        const a =strSim.findBestMatch(element,dict)
+        const a = strSim.findBestMatch(element, dict)
         finalArray.push(a.bestMatch.target)
-    
+
     }
     return finalArray
-}
+};
 
-//Define the function
+//Data pre-processing
 const dataPrep = (text) => {
     //Convert string to standard lexicons
     const toLex = standardLex(text);
     //Convert all to lower case
     const toLow = toLex.toLowerCase();
+    //Normalize
+    const normalized = toLow.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     //remove numbers and punctuations
-    const alphaOnly = toLow.replace(/[^a-zA-Z\s]+/g, '');
+    const alphaOnly = normalized.replace(/[^a-zA-Z\s]+/g, '');
     //convert string to lexicons again
-    const toLex2 = standardLex(alphaOnly)
+    const toLex2 = standardLex(alphaOnly);
     //Tokenize strings
-    const tokenized = tokenizer.tokenize(toLex2)
+    const tokenized = tokenizer.tokenize(toLex2);
     //Remove stopwords
     const remSw = stWord.removeStopwords(tokenized);
-
-    
     // return the final result
- return bestMatch(remSw)
-}
+    return bestMatch(remSw);
+};
 
+//Normalization
+const countryNormalization = (countryName) => {
+    return countryName.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
 
-module.exports={dataPrep}
+module.exports = { dataPrep, countryNormalization };
 
 
 
--- routes/tweet.js
+++ routes/tweet.js
@@ -6,8 +6,8 @@
 const express = require('express');
 const router = express.Router();
 const Twit = require('twit')
-const fs= require('fs');
-require('events').EventEmitter.defaultMaxListeners = 1000;
+const cL = require('country-list');
+
 //Gloabl variables
 const creds = require('../creds/tweetapiKey');
 
@@ -21,33 +21,37 @@ const tweetDB = dbConnection.model('tweet');
 
 //Create a new Twitter crawler instancec
 const T = new Twit(creds);
-const stream = T.stream('statuses/filter', { track: ['covid19', 'coronavirus'], language: 'en' })
-
+const stream = T.stream('statuses/filter', { track: ['covid19', 'coronavirus', 'CoronaVirusUpdates', 'COVIDー19', 'QuaratineLife', 'Quaratine', 'lockdown', 'self-isolate', 'social-distancing'] })
+const cities = require('all-the-cities');
+// console.log(cities.filter(city => {
+//     city.name.match('Amsterdam') && city.country.match('NL')
+// }));
 //Tweet Stream On
-stream.on('tweet', (twt) => {
-if(twt.user.location!=null){
-        //Tweet Object to be stored in the db
-        let twitObj = {
-            date: twt.created_at,
-            text: dataPrep(twt.text),
-            location: twt.user.location
-        }
-    console.log(twitObj)
-        // //Save the object into the db
-        //  new tweetDB(twitObj)
-        //     .save()
-        //      .then(rs => console.log(rs))
-}
-})
+// stream.on('tweet', (twt) => {
+// if(twt.user.location!=null){
+//         //Tweet Object to be stored in the db
+//         let twitObj = {
+//             date: twt.created_at,
+//             text: dataPrep(twt.text),
+//             location: twt.user.location
+//         }
+//     console.log(twitObj.location)
+//         // //Save the object into the db
+//         //  new tweetDB(twitObj)
+//         //     .save()
+//         //      .then(rs => console.log(rs))
+// }
+// })
 
 // Tweet Stream On
 // stream.on('tweet',(twt)=>{
 // if(twt.user.location!=null){
-//     console.log(twt.user.location)
+//     // console.log(twt.user.location.split(","))
+//  console.log(twt.user.location)
 // }
 // })
 
-
+// console.log(cL.getCode('Perú'));
 
 //Export the Module
 module.exports = router;
@@ -74,3 +78,5 @@ module.exports = router;
     // console.log(twt.lang)
     // console.log(twt.user.location)
     // console.log(twt)
+
+    // '肺炎','新冠病毒','新冠肺炎','病毒','新型冠状病毒'
