f90da7ea7b222c65172b2d4bb36617a1302938ff
Algorithm <11154774+algo7@users.noreply.github.com>
2020-03-29 17:41:57 +0800

    node module custom config bash automation

---
+++ nodeModuleconfig/nodeCus.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp stopwords_en.js ../node_modules/stopword/lib/stopwords_en.js && cp data.json ../node_modules/country-list/data.json && cp wordDict.js ../node_modules/apos-to-lex-form/wordDict.js
