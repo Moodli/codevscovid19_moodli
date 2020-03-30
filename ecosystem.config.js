58fc664d6e3289f6235cbd027255c293474a089f
Algorithm <11154774+algo7@users.noreply.github.com>
2020-03-30 17:59:28 +0800

    update deployment config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,11 @@ module.exports = {
     apps: [{
         name: 'moodliBackend',
         script: 'app.js',
-        instances: 1,
+        instances: 0,
         autorestart: true,
         watch: false,
         max_memory_restart: '1G',
-        exec_mode: 'fork',
+        exec_mode: 'cluster',
         env: {
             NODE_ENV: 'development'
         },
