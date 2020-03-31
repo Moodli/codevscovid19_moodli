d9fd3a8f6ea16ef25a7fd6fd57788fc91ee49eab
Algorithm <11154774+algo7@users.noreply.github.com>
2020-03-31 20:47:59 +0800

    update deployment config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
         instances: 1,
         autorestart: true,
         watch: false,
-        max_memory_restart: '1G',
+        max_memory_restart: '5G',
         exec_mode: 'fork',
         env: {
             NODE_ENV: 'development'
