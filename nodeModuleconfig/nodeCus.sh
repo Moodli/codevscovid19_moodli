c71aad58c168822d95ad27d1f8a617ab12fc0cb3
Algorithm <11154774+algo7@users.noreply.github.com>
2020-06-02 20:02:43 +0800

    nodecus

--- nodeModuleconfig/nodeCus.sh
+++ nodeModuleconfig/nodeCus.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo cp stopwords_en.js ../node_modules/stopword/lib/stopwords_en.js && cp data.json ../node_modules/country-list/data.json && cp wordDict.js ../node_modules/apos-to-lex-form/wordDict.js && echo node module customization done
+cp stopwords_en.js ../node_modules/stopword/lib/stopwords_en.js && cp data.json ../node_modules/country-list/data.json && cp wordDict.js ../node_modules/apos-to-lex-form/wordDict.js && echo node module customization done
