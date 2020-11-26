5e4dbf234112265762601a280a2eb9f60424849a
thealgorithm <11154774+algo7@users.noreply.github.com>
2020-11-26 17:32:46 +0100

    change comment format

--- config/csvGen.js
+++ config/csvGen.js
@@ -1,89 +1,89 @@
 
-//Dependencies
+// Dependencies
 const { threadId, parentPort, } = require('worker_threads');
 const fs = require('fs');
 const path = require('path');
 
-//Winston Loggers
+// Winston Loggers
 const logger = require('./logs');
 const workerLog = logger.get('workerLog');
 workerLog.info(`CSV ${threadId} Started`);
 
-//Write Stream Parameters
+// Write Stream Parameters
 const csvLocation = path.join(__dirname, '../mlModel/tweets.csv');
 const writeSt = fs.createWriteStream(csvLocation, { flags: 'a', });
 
 const csvFunc = (twt) => {
 
-    //Create arrays to hold processed texts
+    // Create arrays to hold processed texts
     const textHumanArray = [];
     const textArray = [];
     const locationArray = [];
 
-    //Iterate through the twitObject
+    // Iterate through the twitObject
     for (const key in twt) {
 
         if (key === 'text') {
 
-            //Get the column name
+            // Get the column name
             let element = twt[key];
 
-            //Add double quotes to escape the string
+            // Add double quotes to escape the string
             element = element.map(x => `""${x}""`);
 
-            //Push the processed text to the array
+            // Push the processed text to the array
             textArray.push(`"[${element}]"`);
         }
 
-        //Get the coordinates
+        // Get the coordinates
         if (key === 'location') {
 
-            //Get the column name
+            // Get the column name
             let element = twt[key];
 
-            //Push the processed text to the array
+            // Push the processed text to the array
             locationArray.push(`"[${element}]"`);
         }
 
 
         if (key === 'textHuman') {
 
-            //Get the column name
+            // Get the column name
             let elementx = twt[key];
 
-            //Remove csv sensitive characters
+            // Remove csv sensitive characters
             let elementy = elementx.replace(/\n/g, ' ').replace(/,/g, ' ').replace(/"/g, ' ');
 
-            //Push the processed text to the array
+            // Push the processed text to the array
             textHumanArray.push(`"${elementy}"`);
 
         }
     }
 
-    //Join the the rows
+    // Join the the rows
     const csv = `${'\n'}${textArray},${locationArray},${textHumanArray}`;
 
-    //Write to the file
+    // Write to the file
     writeSt.write(csv);
     return;
 
 };
 
-//Listening for messages from the parent
+// Listening for messages from the parent
 parentPort.on('message', data => {
     csvFunc(data);
 });
 
 
- // //Write to the disk
-    // fs.writeFileSync('./x.csv', csv);
-    // return csv
+ //  // Write to the disk
+    //  fs.writeFileSync('./x.csv', csv);
+    //  return csv
 
-     // const csv = `${keys.join(',')}${'\n'}${textHumanArray}`;
-    // const csv = `text,textHuman,location${'\n'}${textHumanArray}`;
-    // const csv = `${'\n'}${textArray},${locationArray}`;
+     //  const csv = `${keys.join(',')}${'\n'}${textHumanArray}`;
+    //  const csv = `text,textHuman,location${'\n'}${textHumanArray}`;
+    //  const csv = `${'\n'}${textArray},${locationArray}`;
 
-    // let elementx = twt[key];
-            // let element = elementx.toString();
-            // let elementClean = element.replace(/[^a-zA-Z\s]+/g, ' ')
-            // textHumanArray.push(`"${elementClean}"`);
+    //  let elementx = twt[key];
+            //  let element = elementx.toString();
+            //  let elementClean = element.replace(/[^a-zA-Z\s]+/g, ' ')
+            //  textHumanArray.push(`"${elementClean}"`);
